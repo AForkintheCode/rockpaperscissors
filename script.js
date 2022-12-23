92d43caa36cbce3fa3c31324079e3ede907281d2
shavanapuchta <119765380+shavanapuchta@users.noreply.github.com>
2022-12-22 20:57:24 -0800

    added-index-file-and-script-js

---
+++ script.js
@@ -0,0 +1,17 @@
+var myChoice = ["R", "P", "S"]
+var computerChoice = ["R", "P", "S"]
+
+let myInput = window.prompt("What do you choose?");
+if (myInput.toUpperCase() = "R", "S", "P")
+
+
+var myChoice = window.confirm("Are you sure?");
+
+
+    myChoice
+    window.alert("This is my alert")
+
+console.log(myInput())
+
+
+
