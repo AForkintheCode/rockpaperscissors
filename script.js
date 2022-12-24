539751a9f4f05becead390c99fd9e07e0ae690dd
Five <cityslicker86@gmail.com>
2022-12-24 13:20:37 -0800

    rps v 0.1

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-var myChoice = ["R", "P", "S"]
-var computerChoice = ["R", "P", "S"]
+//play again
+var wins = 0;
+var ties = 0;
+var losses = 0
 
-let myInput = window.prompt("What do you choose?");
-if (myInput.toUpperCase() = "R", "S", "P")
+options = ["R", "P", "S"]
 
+var start = confirm("Would you like to play a game?")
+    if(start){        
+        window.alert("Get ready to throw down!")
+        playGame()
+    }
+    else{
+        window.alert("Thank you come again.")
+    }
 
-var myChoice = window.confirm("Are you sure?");
+function playGame(){
+            playerChoice = window.prompt("Rock (R), paper (P), or scissors(S)?")
+            playerChoice = playerChoice.toUpperCase()
+            var compChoice = options[Math.floor(Math.random()*options.length)]
+            if (playerChoice === compChoice){
+                console.log("It's a tie!")
+                ties++
+            }
+            else if (playerChoice == "R" && compChoice == "S" || playerChoice == "P" && compChoice == "R" || playerChoice == "S" && compChoice == "P"){
+                console.log("It's a win!")
+                wins++
+            }
+            else {
+                console.log("Computer wins!")
+                losses++
+            }
 
-
-    myChoice
-    window.alert("This is my alert")
-
-console.log(myInput())
-
-
+            window.alert("Computer chose " + compChoice)
+            window.alert("Wins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
+            let playAgain = confirm("Would you like to play again?")
+            if (playAgain === true) {
+                playGame()
+                }
+            else {
+                window.alert("Thanks for playing!")
+            } 
+            return;
+        }
 
